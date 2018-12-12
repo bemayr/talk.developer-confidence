993d4160af4566316feb66d0a21713a5b9cfd073
Bernhard Mayr <bernhard@mayr.io>
2018-12-12 22:22:01 +0100

    [examples]: added statements-vs-expressions/ExpressionsAllTheWay

---
+++ examples/statements-vs-expressions/ExpressionsAllTheWay/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpressionsAllTheWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IfThenElseStatement(true);
+            LoopStatement();
+        }
+
+        public static void IfThenElseStatement(bool aBool)
+        {
+            int result;
+
+            if (aBool)
+            {
+                result = 42;
+                Console.WriteLine("result={0}", result);
+            }
+        }
+
+        public static void LoopStatement()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+            int sum = 0;
+
+            for (var i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("sum={0}", sum);
+        }
+    }
+}
---
+++ examples/statements-vs-expressions/ExpressionsAllTheWay/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpressionsAllTheWay
+{
+    public class Solution
+    {
+        public void IfThenElseExpression(bool aBool)
+        {
+            int result = aBool ? 42 : 0;
+            Console.WriteLine("result={0}", result);
+        }
+
+        public void LoopStatementForEach()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+            int sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine("sum={0}", sum);
+        }
+
+        public void LoopExpressionAggregate()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            var sum = numbers.Aggregate(0, (sumSoFar, i) => sumSoFar + i);
+
+            Console.WriteLine("sum={0}", sum);
+        }
+
+        public void LoopExpressionSum()
+        {
+            var numbers = new List<int> { 1, 2, 3 };
+
+            var sum = numbers.Sum();
+
+            Console.WriteLine("sum={0}", sum);
+        }
+    }
+}
